545ce41e402df9bf6aeb49c4ba31efa33f080475
leejojosepj <leejojosepj@gmail.com>
2021-08-09 21:44:45 +0530

    initial commit

---
+++ src/main/java/com/sonarjystudy/SonarcloudLeejoWorkApplication.java
@@ -0,0 +1,13 @@
+package com.sonarjystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudLeejoWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudLeejoWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonarjystudy/SonarcloudLeejoWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarjystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudLeejoWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
