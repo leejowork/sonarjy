bf6847cb909007cfeb2dc8f4f747c3430261d590
leejojosepj <leejojosep@gmail.com>
2021-08-10 14:09:54 +0530

    first unit test for wecome controller

---
+++ src/test/java/com/sonarjystudy/controller/WelcomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.sonarjystudy.controller;
+
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(WelcomeController.class)
+public class WelcomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void welcomeMap_basic() throws Exception {
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+														.get("/welcome/jesus")
+														.accept(MediaType.APPLICATION_JSON);
+		MvcResult result = mockMvc.perform(requestBuilder)
+								.andExpect(status().isOk())
+								.andExpect(content().string("jesus welcome"))
+								.andReturn();
+		
+	}
+	
+}
