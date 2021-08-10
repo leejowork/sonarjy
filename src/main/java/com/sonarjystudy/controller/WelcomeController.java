fa3d57a9c481a0b0581169737466b11616121225
leejojosepj <leejojosep@gmail.com>
2021-08-10 18:31:30 +0530

    first unit test for wecome controller assert inclunde9

--- src/main/java/com/sonarjystudy/controller/WelcomeController.java
+++ src/main/java/com/sonarjystudy/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.sonarjystudy.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,7 @@ public class WelcomeController {
 
 	
 	@GetMapping("/jesus")
-	public String welcomeMap(){
-		return "jesus welcome";
+	public ResponseEntity<String> welcomeMap(){
+		return new ResponseEntity<>("jesus welcome",HttpStatus.OK);
 	}
 }
